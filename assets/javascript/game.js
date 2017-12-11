d553f29ddb93c26f6e01a394401f04c8d4622603
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2017-12-11 13:46:48 -0500

    worked on the html and css files and wrote some pseudocode for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+
+// Global variables
+
+
+
+
+// Function to start game
+
+    
+  
+
+// To make pictures clickable
+
+$("#cersei").click(function () {
+    
+});
+
+$("#daenerys").click(function () {
+    
+});
+
+$("#jonsnow").click(function () {
+   
+});
+
+$("#nightking").click(function () {
+   
+});
+
+// To move chosen character to "your character" section and enemies to the "enemies available to fight" section
+
+
+// To move the chosen enemy to the "fight section"
+
+
+// Attack button to play game
+
+
+// Function to check if enemy is defeated or not
+
+
+// If enemy is defeated, remove that enemy from page and player is able to choose the next enemy which is moved to the "fight section"
+
+
+// If player is defefated, game is over and button to restart game appears
+
+
+
+// After player wins game, button for the restart game appears
+
+
+// Function to reset game working by clicking restart button
+
+
+
+// Ready function
+
